87a610e39e01fbf388e7dbb49b491c622fc4f4da
salema <114165491+vsalema@users.noreply.github.com>
2026-01-20 23:48:26 +0100

    Update tron-ares.ios-inline.js

--- tron-ares.ios-inline.js
+++ tron-ares.ios-inline.js
@@ -3935,7 +3935,7 @@ document.addEventListener('click', () => closeAllTrackMenus());
 // =====================================================
 
 // URLs playlists (identiques à l'init)
-const MAIN_PLAYLIST_URL = "playlist-tron_tmdb_ranged.m3u";
+const MAIN_PLAYLIST_URL = "playlist-tron_tmdb_ranged-1.fixed.sorted.m3u";
 const FR_PLAYLIST_URL   = "https://vsalema.github.io/tvpt4/css/playlist-tvf-r.m3u";
 
 // ⏱️ Auto-refresh: change la valeur si tu veux (en ms)
